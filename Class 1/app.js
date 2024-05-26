ca6cbf263c6135d7475960acf638c09761acfcaa
Noman Shamim <nomanshamim2004@gmail.com>
2024-05-26 17:44:02 +0500

    Working on Signup using "fs"

--- Class 1/app.js
+++ Class 1/app.js
@@ -8,16 +8,16 @@ const filePath = path.join(process.cwd(), 'data.json')
 
 const Server = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.write("HOME ROUTE!")
+        res.write(`<h1>HOME ROUTE!</h1>`)
         res.end()
         return
     }
     if (req.url === '/login') {
         res.setHeader('Content-Type', 'text/html')
-        res.write(`<form action="/submit method="POST">
+        res.write(`<h1>Login</h1> <form action="/submit" method="POST">
             <input type="text" name="username" placeholder="Enter username"/></br>
             <input type="password" name="password" placeholder="Enter password"/></br>
-            <button>Submit</button>
+            <button type="submit">login</button>
             </form>`)
         res.end()
         return
@@ -39,6 +39,54 @@ const Server = http.createServer((req, res) => {
             })
             res.write('Success')
             res.end()
+        })
+        return
+    }
+    if (req.url === '/signup') {
+        res.write(`<h1>Sign Up</h1>
+        <form action="/submit" method="POST">
+                <input type="text" name="username" placeholder="Enter username"/></br>
+                <input type="password" name="password" placeholder="Enter password"/></br>
+                <input type="password" name="confirm_password" placeholder="Enter confirm password"/></br>
+                <button type="submit">sign up</button>
+        </form>`)
+        res.end()
+        return
+    }
+    if (req.url === '/submit') {
+        let data = ''
+        req.on('data', (chunk) => {
+            data += chunk
+        })
+        req.on('end', () => {
+            let parse = queryString.parse(data)
+            if (parse.password === parse.confirm_password) {
+
+                fs.readFile(filePath, (err, data) => {
+                    if (err) {
+                        console.log("singnUp err---->", err);
+                        return
+                    }
+                    const jsondata = JSON.parse(data)
+                    const NewUser = {
+                        id: jsondata.users.length + 1,
+                        username: parse.username,
+                        password: parse.password
+                    }
+
+                    jsondata.users.push(NewUser)
+
+                    fs.writeFile(filePath, JSON.stringify(jsondata, 2), (err) => {
+                        if (err) {
+                            console.log("JsonData--->", err)
+                        }
+                        else {
+                            res.end('signUp successfully')
+                            return
+                        }
+                    })
+                })
+            }
         })
         return
     }
