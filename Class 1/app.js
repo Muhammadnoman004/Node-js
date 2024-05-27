ed6a63bc88f30cc76daa6ed1fce30b36f4e60ab7
Noman Shamim <nomanshamim2004@gmail.com>
2024-05-27 22:43:12 +0500

    Signup done..

--- Class 1/app.js
+++ Class 1/app.js
@@ -44,7 +44,7 @@ const Server = http.createServer((req, res) => {
     }
     if (req.url === '/signup') {
         res.write(`<h1>Sign Up</h1>
-        <form action="/submit" method="POST">
+        <form action="/submit2" method="POST">
                 <input type="text" name="username" placeholder="Enter username"/></br>
                 <input type="password" name="password" placeholder="Enter password"/></br>
                 <input type="password" name="confirm_password" placeholder="Enter confirm password"/></br>
@@ -53,21 +53,21 @@ const Server = http.createServer((req, res) => {
         res.end()
         return
     }
-    if (req.url === '/submit') {
+    if (req.url === '/submit2') {
         let data = ''
         req.on('data', (chunk) => {
             data += chunk
         })
         req.on('end', () => {
             let parse = queryString.parse(data)
-            if (parse.password === parse.confirm_password) {
-
-                fs.readFile(filePath, (err, data) => {
+            if (parse.password == parse.confirm_password) {
+                fs.readFile(filePath, (err, fileData) => {
                     if (err) {
                         console.log("singnUp err---->", err);
                         return
                     }
-                    const jsondata = JSON.parse(data)
+                    const jsondata = JSON.parse(fileData);
+                    console.log("json data ==>", jsondata);
                     const NewUser = {
                         id: jsondata.users.length + 1,
                         username: parse.username,
@@ -76,22 +76,26 @@ const Server = http.createServer((req, res) => {
 
                     jsondata.users.push(NewUser)
 
-                    fs.writeFile(filePath, JSON.stringify(jsondata, 2), (err) => {
+                    fs.writeFile(filePath, JSON.stringify(jsondata, null, 2), (err) => {
                         if (err) {
                             console.log("JsonData--->", err)
                         }
                         else {
-                            res.end('signUp successfully')
+                            res.end('<h1>SignUp Successfully!</h1>')
                             return
                         }
                     })
                 })
             }
+            else {
+                res.end("<h1>Please Confirm Password!</h1>")
+                return
+            }
         })
         return
     }
 
-    res.write("INVALID ROUTE!")
+    res.write("<h1>INVALID ROUTE!</h1>")
     res.end()
 })
 
